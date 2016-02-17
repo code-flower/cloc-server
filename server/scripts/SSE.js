51d989594ad32e538cb6a71b6935dd9b40d88dbc
Jake Mensch <jake.mensch@gmail.com>
2016-02-17 07:31:37 -0800

    nodejs-websockets working between server and client

--- server/scripts/SSE.js
+++ server/scripts/SSE.js
@@ -1,69 +1,59 @@
+
+
+
+//////////////////// WEBSOCKETS IMPLEMENTATION ////////////////////
+
+var ws = require('nodejs-websocket');
+
+var server = ws.createServer(function(conn) {
+  console.log("New connection")
+  conn.on("text", function (str) {
+    console.log("Received "+str)
+    conn.sendText(str.toUpperCase()+"!!!")
+  })
+  conn.on("close", function (code, reason) {
+    console.log("Connection closed")
+  })
+}).listen(8001);
+
+
+
+
+
+
+
+
+/////////////////// EVENT SOURCE IMPLEMENTATION ///////////////////
 // this file implements the server side of the EventSource protocol
 // eventually need to convert this to websockets to avoid Sophos issue
 
 ////////// PRIVATE /////////
 
-function SSE(httpResponse) {
-  this.conn = httpResponse;
-  this.conn.writeHead(200, {
-    'Content-Type':  'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection':    'keep-alive',
-    'Access-Control-Allow-Origin': '*'
-  });
-  console.log("SSE CONNECTION OPEN");
-};
+// function SSE(httpResponse) {
+//   this.conn = httpResponse;
+//   this.conn.writeHead(200, {
+//     'Content-Type':  'text/event-stream',
+//     'Cache-Control': 'no-cache',
+//     'Connection':    'keep-alive',
+//     'Access-Control-Allow-Origin': '*'
+//   });
+//   console.log("SSE CONNECTION OPEN");
+// };
 
-SSE.prototype.write = function(data) {
-  console.log(data);
-  this.conn.write('id: node' + '\n');
-  this.conn.write('data: ' + data + '\n\n');
-};
+// SSE.prototype.write = function(data) {
+//   console.log(data);
+//   this.conn.write('id: node' + '\n');
+//   this.conn.write('data: ' + data + '\n\n');
+// };
 
-SSE.prototype.close = function() {
-  this.conn.end();
-  this.conn = null;
-  console.log("SSE CONNECTION CLOSED");
-};
+// SSE.prototype.close = function() {
+//   this.conn.end();
+//   this.conn = null;
+//   console.log("SSE CONNECTION CLOSED");
+// };
 
 ////////// PUBLIC //////////
 
-module.exports = SSE;
+// module.exports = SSE;
 
-
-
-
-//////////// OLD VERSION ////////////
-
-// the sse connection
-// var conn = null;
-
-// module.exports = {
-
-//   // open the connection
-//   open: function(httpResponse) {
-//     conn = httpResponse;
-//     conn.writeHead(200, {
-//       'Content-Type':  'text/event-stream',
-//       'Cache-Control': 'no-cache',
-//       'Connection':    'keep-alive',
-//       'Access-Control-Allow-Origin': '*'
-//     });
-//     console.log("SSE CONNECTION OPEN");
-//   },
-
-//   // stream a bit of data
-//   write: function(data) {
-//     console.log(data);
-//     conn.write('id: node' + '\n');
-//     conn.write('data: ' + data + '\n\n');
-//   },
-
-//   // close the connection
-//   close: function() {
-//     conn.end();
-//     conn = null;
-//     console.log("SSE CONNECTION CLOSED");
-//   }
-// };
 
