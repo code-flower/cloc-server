da892682b6719296921a1f7928acf7f5c9a73e7e
Jake Mensch <jake.mensch@gmail.com>
2016-02-20 19:21:32 -0800

    gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
 var nodemon = require('gulp-nodemon');
 var browserSync = require('browser-sync').create();
 var open = require('gulp-open');
-var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var babelify = require('babelify');
 
 /////////////// SUB-TASKS ///////////////////
 
 gulp.task('bundle', function() {
   return browserify('./client/js/index.js')
-    .transform(babelify, {presets: ["es2015"]})
-    .on('error', function(err) {console.log(err);})
+    .transform(babelify, { presets: ["es2015"] })
+    .on('error', function(err) { console.log(err); })
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('./client/js/'))
@@ -21,7 +21,7 @@ gulp.task('bundle', function() {
 /////////// DEFAULT TASK COMPONENTS /////////
 
 gulp.task('watch:server', function() {
-  nodemon({
+  return nodemon({
     script: 'server/server.js',
     ext: 'js',
     ignore: [
@@ -30,30 +30,25 @@ gulp.task('watch:server', function() {
       'server/samples/**',
       'gulpfile.js'
     ]
-  }).on('restart', function() {
-    setTimeout(function() {
-      browserSync.reload();
-    }, 1000);
-  });
+  }).on('start', browserSync.reload);
 });
 
 gulp.task('watch:client', function() {
   gulp.watch(['./client/**', '!./client/js/bundle.js'], ['bundle']);
 });
 
-gulp.task('browser-sync', function() {
-  browserSync.init({ ui: { port: 8090 } });
-});
+gulp.task('open-browser', ['bundle'], function() {
+  browserSync.init({ 
+    ui: { port: 8090 } 
+  });
 
-gulp.task('open-chrome', ['bundle'], function() {
-  gulp.src('')
-    .pipe(open({
-      app: 'google chrome', 
-      uri: 'http://localhost:8000'
-    }));
+  gulp.src('').pipe(open({
+    app: 'google chrome', 
+    uri: 'http://localhost:8000'
+  }));
 });
 
 /////////////// DEFAULT TASK ///////////////
 
-gulp.task('default', ['watch:server', 'watch:client', 'browser-sync', 'open-chrome']);
+gulp.task('default', ['watch:server', 'watch:client', 'open-browser']);
 
