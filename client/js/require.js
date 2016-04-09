7997e6e6739c1c4521c270d691d68e13cfe8b60e
Jake Mensch <jake.mensch@gmail.com>
2016-04-09 12:24:33 -0700

    reorganize require.js

--- client/js/require.js
+++ client/js/require.js
@@ -8,25 +8,25 @@ require('./app/app');
 // directives
 require('./app/directives/appContainer');
 require('./app/directives/flowerControl');
+require('./app/directives/flowerLanguages');
 require('./app/directives/flowerTerminal');
 require('./app/directives/flowerViz');
-require('./app/directives/flowerLanguages');
 require('./app/directives/ignoredFiles');
 require('./app/directives/userPrefs');
 
 // factories
-require('./app/factories/WS');
-require('./app/factories/HTTP');
 require('./app/factories/CodeFlower');
+require('./app/factories/dataService');
 require('./app/factories/dbAccess');
 require('./app/factories/flowerUtils');
-require('./app/factories/dataService');
+require('./app/factories/HTTP');
 require('./app/factories/state');
 require('./app/factories/userPrefsService');
+require('./app/factories/WS');
 
 // controllers
-require('./app/controllers/dispatcher');
 require('./app/controllers/credentialsModal');
+require('./app/controllers/dispatcher');
 require('./app/controllers/mainModal');
 
 
