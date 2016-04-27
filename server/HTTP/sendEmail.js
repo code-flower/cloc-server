4193a78ae4d6899722ae22ab4b609738eae9b94f
Jake Mensch <jake.mensch@gmail.com>
2016-04-27 13:50:59 -0700

    fixed gmail issue

--- server/HTTP/sendEmail.js
+++ server/HTTP/sendEmail.js
@@ -5,44 +5,16 @@ var send = require('gmail-send');
 
 //////////// EXPORTS ////////////
 
-// module.exports = function sendEmail(response, message) {
-//   send({
-
-//     user:    user.email,
-//     pass:    user.password, 
-//     to:      user.email,
-//     subject: 'message for codeflower.la',
-//     text:    message
-
-//   })({}, function(err, res) {
-
-//     if (err) {
-//       console.log("EMAIL ERROR:", err);
-//       response.writeHead(500, {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Origin': '*'
-//       });
-//       response.end(JSON.stringify(err));
-//     } else {
-//       console.log("SENT EMAIL:", message);
-//       response.writeHead(200, {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Origin': '*'
-//       });
-//       response.end(JSON.stringify(message));
-//     }
-
-//   });
-// };
-
-// TEMPORARY
-// need to get gmail working on the server
-// until then write to a file
-var fs = require('fs');
 module.exports = function sendEmail(response, message) {
-  var data =  (new Date()).toString() + '\n' + message + '\n\n'
+  send({
 
-  fs.appendFile('private/emails.txt', data, function(err, res) {
+    user:    user.email,
+    pass:    user.password, 
+    to:      user.email,
+    subject: 'message for codeflower.la',
+    text:    message
+
+  })({}, function(err, res) {
 
     if (err) {
       console.log("EMAIL ERROR:", err);
