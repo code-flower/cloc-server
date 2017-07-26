50a4662fc1821ed1a7696e65f4673b5ebe07b366
Jake Mensch <jake.mensch@gmail.com>
2017-07-26 14:06:35 -0700

    removed non-server code

--- client/js/app/app.js
+++
@@ -1,6 +0,0 @@
-// get the shared config file
-var appConfig = require('../../../shared/appConfig.js');
-
-// inject shared config into angular app
-angular.module('CodeFlower', ['ui.bootstrap'])
-  .constant('appConfig', appConfig);
--- client/js/require.js
+++
@@ -1,8 +0,0 @@
-/* this is the file that browserify bundles */
-
-// vendor
-require('angular');
-require('./vendor/ui-bootstrap-custom-tpls-1.2.1');
-
-// all application js
-require('bulk-require')(__dirname, ['./app/**/*.js']);
