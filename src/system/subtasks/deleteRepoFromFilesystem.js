c5287c11c75868a42a1e50019e669f9897ea49bb
Jake Mensch <jake.mensch@gmail.com>
2017-08-12 09:16:25 -0700

    deleteRepoFromFilesystem working

--- src/system/subtasks/deleteRepoFromFilesystem.js
+++ src/system/subtasks/deleteRepoFromFilesystem.js
@@ -1,6 +1,7 @@
 //////////// IMPORTS ////////////
 
 const Promise = require('bluebird'),
+      rimraf = require('rimraf'),
       config = require('@config');
 
 //////////// PRIVATE ////////////
@@ -9,7 +10,10 @@ function deleteRepoFromFilesystem(repo) {
   return new Promise((resolve, reject) => {
     console.log("7. Deleting Repo From Filesystem");
 
-    resolve(repo);
+    if (!config.deleteAfterClone)
+      resolve(repo);
+    else
+      rimraf(config.paths.repos + repo.folderName, () => resolve(repo));
   });
 }
 
